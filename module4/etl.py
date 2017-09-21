7d9fec6b0dd93cc27a2fc578fc0b78061147d027
Mike Ghen <mike.ghen@gmail.com>
2017-09-21 12:40:54 -0400

    Adding lecture 4 notes

--- module4/etl.py
+++ module4/etl.py
@@ -1,4 +1,3 @@
-
 SMA_WINDOW_SIZE = 5
 
 def extract(filename):
@@ -9,27 +8,13 @@ def extract(filename):
 
     with open(filename, 'r') as data_file:
         for datum in data_file:
-            extract_data.append(datum)
+            extracted_data.append(tuple(datum.strip().split('\t')))
 
-    return extract_data
+    return extracted_data[1:]
 
 def transform(data):
     """
     Transforms a list of x,y values to compute an SMA for y values.
     Returns a list of tuples (x,y') where y' is the SMA value computed
     """
-    transformed_data = [] # The SMA data
-    for index, datum in enumerate(data):
-        if index < SMA_WINDOW_SIZE:
-            transformed_data.append((datum[0],0))
-        else:
-            sma = 0
-            for i in range(SMA_WINDOWS_SIZE):
-                sma += data[index - i]
-            sma = sma/SMA_WINDOWS_SIZE
-            transformed_data.append()
-    return
-
-def load(data, destination):
-    # Load funciton: write data to a new file
     return
--- module4/testing.py
+++ module4/testing.py
@@ -1,11 +1,29 @@
-from etl import extract, transform, load
+from etl import extract, transform
 import unittest
 
 test_file_path = './data/OPEC_ORB_TEST.csv'
 
-extract_data = []
+extract_data = [
+('9/18/17', '53.78'),
+('9/15/17', '53.64'),
+('9/14/17', '53.63'),
+('9/13/17', '52.92'),
+('9/12/17', '52.08'),
+('9/11/17', '51.82'),
+('9/8/17', '52.53'),
+('9/7/17', '52.48'),
+('9/6/17', '52.04')]
 
-transform_data = []
+transform_data = [
+('9/18/17', 0),
+('9/15/17', 0),
+('9/14/17', 0),
+('9/13/17', 0),
+('9/12/17', 0),
+('9/11/17', 52.818),
+('9/8/17', 52.596000000000004),
+('9/7/17', 52.366),
+('9/6/17', 52.19)]
 
 class MyTest(unittest.TestCase):
     def test_extract(self):
@@ -16,8 +34,6 @@ class MyTest(unittest.TestCase):
         opec_orb_transform_data = transform(extract_data)
         assert(transform_data==opec_orb_transform_data)
 
-    def test_load(self):
-        assert(1==0)
 
 if __name__ == '__main__':
     unittest.main()
